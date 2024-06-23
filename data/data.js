f9930cefffd059e54c96505c19578a47a556a6ad
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-06-23 10:25:32 -0300

    array complete

---
+++ data/data.js
@@ -0,0 +1,11 @@
+const arrayTecnologias = [
+    'JavaScript', 'Python', 'TypeScript', 'Java',
+    'Node', 'React', 'Git', 'Ruby', 'Kotlin',
+    'HTML', 'PHP', 'SQL', 'Csharp',
+    'Shell', 'Go', 'Assembly', 'Rust',
+    'Swift', 'C', 'Zed', 'CSS', 'Blazor',
+    'Spring', 'Angular', 'Vue', 'Bootstrap',
+    'JQuery', 'Ember', 'Spark', 'Struts', 'Django'
+];
+
+export default arrayTecnologias;

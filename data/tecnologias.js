4ed7d7968b72fe6c2a357460bbb0c83f18fd7515
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-06-24 20:52:23 -0300

    change local folder

--- data/tecnologias.js
+++ data/tecnologias.js
@@ -1,13 +1,13 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
-async function criarEinserirTecnologia(tecnologia) {
+async function criarEinserirTecnologia(nome) {
     const db = await open({
         filename: '../banco.db',
         driver: sqlite3.Database,
     });
 
-    db.run('')
+    db.run(`CREATE TABLE IF NOT EXIST tecnologias (id INTEGER PRIMARY KEY, nome TEXT) `);
 }
 
 criarEinserirTecnologia();

38d1b850964fa1e9b5ad66433903512a3e766110
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-06-29 20:33:03 -0300

    feat: use await comand with try/catch for better syntax

--- data/tecnologias.js
+++ data/tecnologias.js
@@ -2,14 +2,20 @@ import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
 async function criarEinserirTecnologia(nome) {
-    const db = await open({
-        filename: './data/banco.db',
-        driver: sqlite3.Database,
-    });
+    try {
+        const db = await open({
+            filename: './data/banco.db',
+            driver: sqlite3.Database,
+        });
+        
+        await db.run(`CREATE TABLE IF NOT EXISTS tecnologias (id INTEGER PRIMARY KEY, nome TEXT) `);
 
-    db.run(`CREATE TABLE IF NOT EXISTS tecnologias (id INTEGER PRIMARY KEY, nome TEXT) `);
-
-    db.run(`INSERT INTO tecnologias (nome) VALUES (?)`, [nome]);
+        await db.run(`INSERT INTO tecnologias (nome) VALUES (?)`, [nome]);
+        console.log(`Tecnologia ${nome} inserida com sucesso!`);
+        
+    } catch (error) {
+        console.error(`Erro ao abrir ou manipular o banco de dados`, error);
+    }
 }
 
 criarEinserirTecnologia('Go');

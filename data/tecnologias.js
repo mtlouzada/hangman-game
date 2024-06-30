14c0709c7630fa15114488662ab2f2bbda5550dd
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-06-30 14:59:03 -0300

    feat: try bug fixed sqlite insert

--- data/tecnologias.js
+++ data/tecnologias.js
@@ -1,21 +1,28 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
+import arrayTecnologias from '../data/data.js';
 
-async function criarEinserirTecnologia(nome) {
+async function criarEinserirTecnologia(arrayTecnologias) {
     try {
         const db = await open({
             filename: './data/banco.db',
             driver: sqlite3.Database,
         });
         
-        await db.run(`CREATE TABLE IF NOT EXISTS tecnologias (id INTEGER PRIMARY KEY, nome TEXT) `);
+        for (const tecnologia of arrayTecnologias) 
+        {
+            await db.run(`INSERT INTO tecnologias (nome) VALUES (?)`, [tecnologia]);
+            console.log(`Tecnologia ${tecnologia} inserida com sucesso!`);
+        }
+
+        await db.run(`CREATE TABLE IF NOT EXISTS tecnologias (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT) `);
 
         await db.run(`INSERT INTO tecnologias (nome) VALUES (?)`, [nome]);
         console.log(`Tecnologia ${nome} inserida com sucesso!`);
-        
+
     } catch (error) {
         console.error(`Erro ao abrir ou manipular o banco de dados`, error);
     }
 }
 
-criarEinserirTecnologia('Go');
+criarEinserirTecnologia(arrayTecnologias);

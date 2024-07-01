f15d70f8e17fc4a1aaa0e40eb3c8ad9ee0b67f90
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-07-01 13:56:29 -0300

    feat: erro cannot fild FIXED

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Hangman Tech ⚠️
 
 ## Status: Developing ⚠️
+
+
+#### Para rodar esse projeto rode npm install sqlite3 e npm install sqlite antes
--- data/tecnologias.js
+++ data/tecnologias.js
@@ -5,20 +5,18 @@ import arrayTecnologias from '../data/data.js';
 async function criarEinserirTecnologia(arrayTecnologias) {
     try {
         const db = await open({
-            filename: './data/banco.db',
+            filename: '../data/banco.db',
             driver: sqlite3.Database,
         });
+        
+        await db.run(`CREATE TABLE IF NOT EXISTS tecnologias (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT) `);
         
         for (const tecnologia of arrayTecnologias) 
         {
             await db.run(`INSERT INTO tecnologias (nome) VALUES (?)`, [tecnologia]);
             console.log(`Tecnologia ${tecnologia} inserida com sucesso!`);
+            
         }
-
-        await db.run(`CREATE TABLE IF NOT EXISTS tecnologias (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT) `);
-
-        await db.run(`INSERT INTO tecnologias (nome) VALUES (?)`, [nome]);
-        console.log(`Tecnologia ${nome} inserida com sucesso!`);
 
     } catch (error) {
         console.error(`Erro ao abrir ou manipular o banco de dados`, error);

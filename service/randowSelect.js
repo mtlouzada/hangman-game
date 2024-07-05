0cb2a2ae2bfbd0d6de3576380bd2e73680795807
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-07-05 15:33:34 -0300

    feat: try make firts service bug return error syntax

--- service/randowSelect.js
+++ service/randowSelect.js
@@ -8,7 +8,9 @@ import {open} from 'sqlite';
                 driver: sqlite3.Database,
             });
 
-            
+            const query = "SELECT * tecnologia FROM tecnologias";
+            const randomize = query[Math.floor(Math.random() * query.length)];
+            await db.run(``);
 
         } catch (error) {
             console.error(`Erro ao abrir ou manipular o banco de dados`, error);

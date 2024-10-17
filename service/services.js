64b5b5aa249a9a8e543a9f8309f9fc7720a183cf
mtlouzada <matheuslouzadaa@gmail.com>
2024-10-17 07:31:47 -0300

    delete console.log

--- service/services.js
+++ service/services.js
@@ -4,11 +4,10 @@ export function selectRandomQuestions() {
     const shuffledArray = [...arrayTecnologias].sort(() => 0.5 - Math.random());
     
     return shuffledArray.splice(0, 10);
-    console.log(shuffledArray);
 }
 
 export function showQuestion() {
-    
+
 }
 
 export function validateUserAnswer() {
